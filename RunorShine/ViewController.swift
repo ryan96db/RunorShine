b22fabf70eadb3c39c00bc481afb9087d3bb1902
Ryan DeBose-Boyd <ryan96db@gmail.com>
2018-12-25 21:13:26 -0600

    Initial Commit

---
+++ RunorShine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunorShine
+//
+//  Created by Ryan DeBose-Boyd on 12/25/18.
+//  Copyright © 2018 Ryan DeBose-Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
